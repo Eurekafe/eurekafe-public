3e3dfcfc9730bbe354e03c24ab7646cf412b24b5
syl-gauthier <sylvain.gauthier.2012@gmail.com>
2018-06-06 23:02:04 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -3,22 +3,24 @@
 
 
 "use strict";
+
 require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const momentjs = require("moment");
+const nodemailer = require("nodemailer");
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
-var momentjs = require('moment');
+const port = process.env.PORT||3000;
+const mongoUrl = process.env.MONGO_CRED;
+
 momentjs.locale("fr");
 
-var nodemailer = require("nodemailer");
-
-const path = require("path");
-
-const url = process.env.MONGO_CRED;
-var MongoClient = require("mongodb").MongoClient;
 var dbclient = new Promise(function(resolve, reject) {
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(mongoUrl, function(err, client) {
     if (err) {
       console.log("err");
       reject(err);
@@ -47,44 +49,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public", {maxAge: 3600000}));
-
-app.post("/newsletter", function(req, res) {
-  let newmail = undefined;
-  if (req.body.mail) {
-    newmail = req.body.mail;
-  } else if(req.body.mail2) {
-    newmail = req.body.mail2;
-  }
-  let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (regex.test(newmail)) {
-    var mailOptions = {
-      from: process.env.MAIL,
-      to: process.env.MAIL_TARGET,
-      subject: "inscription newsletter",
-      text: newmail
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-        res.redirect("/error");
-      } else {
-        console.log("Email sent: " + info.response);
-        res.redirect("/newsletterSuccess");
-        dbclient.then(function(dbs) {
-          var collection = dbs.collection("newsletter");
-          collection.insertOne({email: newmail}, function(err, data) {
-            if(err) res.redirect("/error");
-            console.log(data);
-          });
-        });
-        
-      }
-    });
-  } else {
-    res.redirect("/error");
-  }
-  
-});
 
 app.get("/", function(req,res) {
   dbclient.then(function(dbs) {
@@ -127,6 +91,43 @@ app.get("/newsletterSuccess", function(req, res) {
   res.render("newsletter");
 });
 
+app.post("/newsletter", function(req, res) {
+  let newmail = undefined;
+  if (req.body.mail) {
+    newmail = req.body.mail;
+  } else if(req.body.mail2) {
+    newmail = req.body.mail2;
+  }
+  let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regex.test(newmail)) {
+    var mailOptions = {
+      from: process.env.MAIL,
+      to: process.env.MAIL_TARGET,
+      subject: "inscription newsletter",
+      text: newmail
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.redirect("/error");
+      } else {
+        console.log("Email sent: " + info.response);
+        res.redirect("/newsletterSuccess");
+        dbclient.then(function(dbs) {
+          var collection = dbs.collection("newsletter");
+          collection.insertOne({email: newmail}, function(err, data) {
+            if(err) res.redirect("/error");
+            console.log(data);
+          });
+        });
+        
+      }
+    });
+  } else {
+    res.redirect("/error");
+  }
+});
+
 app.get("/events", function (req, res) {
   dbclient.then(function(dbs) {
     var collection = dbs.collection("event");
@@ -156,7 +157,7 @@ app.use(function(req, res) {
   res.status(404).render("404");
 });
 
-const port = process.env.PORT||3000;
+
 
 app.listen(port, function() {
   console.log("\x1b[32m", "app listening on port", port, "\x1b[0m");
